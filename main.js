635cf40c9a8dc686b60e12b80d1dfa04507a1627
Galtico <163339076+Galtico@users.noreply.github.com>
2024-05-22 14:31:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -35,19 +35,16 @@ function calculaTempo(tempoObjetivo){
     minutos %= 60;
     horas %= 24;
     if(tempoFinal > 0){
-        return [dias,horas,minutos,segundos];
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos "; 
     } else{
-        return [0,0,0,0];
+        return"Prazo finalizado";
     }
 
+    
 }
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos [1])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos [1])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos [1])[3];
     for (let i=0; i<contadores.length;i++){
-        //contadores[i].textContent = calculaTempo(tempos[i]);
+        contadores[i].textContent = calculaTempo(tempos[i]);
     }    
 }
 
